6c62e0878f47479db5750c9e9e5f1b179d528cbb
Esbaevnurdos <144226311+Esbaevnurdos@users.noreply.github.com>
2024-12-12 11:50:17 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func getNextID() int {
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Set the correct allowed origin here
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5174") // Allow your frontend origin
+        w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		// Set the allowed methods and headers
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
